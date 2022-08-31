3592354e3829753c6239e28b3305da3ac738c505
nxtdv <frags31560@gmail.com>
2022-08-31 19:20:00 +0200

    🔨 Add direction: dist In the fxmanifest

--- src/fxmanifest.lua
+++ src/fxmanifest.lua
@@ -5,5 +5,5 @@ author "Dev-CasperTheGhost"
 description "TODO"
 version "0.0.0"
 
-server_scripts {"server/server.js"}
-client_scripts {"client/client.js"}
+server_scripts {"dist/server/server.js"}
+client_scripts {"dist/client/client.js"}
